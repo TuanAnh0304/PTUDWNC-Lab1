73b33e05c287ebb3ff89d01d0589e9fe3d1e4657
Vũ Ngọc Tuấn Anh <vungoctuananh030402@gmail.com>
2023-03-01 20:15:00 +0700

    xay dung truy van du lieu

--- src/TipsAndTricks/TatBlog.WinApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -1,7 +1,12 @@
-﻿using TatBlog.Data.Contexts;
+﻿using Microsoft.VisualBasic;
+using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
 
 var context = new BlogDbContext();
+
+IBlogRepository blogRepo = new BlogRepository(context);
+var posts = await blogRepo.GetPopularArticlesAsync(3);
 
 var seeder = new DataSeeder(context);
 
@@ -9,32 +14,32 @@ seeder.Initialize();
 
 var authors = context.Author.ToList();
 
-var posts = context.Posts
-    .Where(p => p.Published)
-    .OrderBy(p => p.Title)
-    .Select(p => new
-    {
-        id = p.Id,
-        title = p.Title,
-        ViewCount = p.ViewCount,
-        PosterDate = p.PostedDate,
-        Author = p.Author.FullName,
-        Category = p.Category.Name
-    })
-    .ToList();
+//var posts = context.Posts
+//    .Where(p => p.Published)
+//    .OrderBy(p => p.Title)
+//    .Select(p => new
+//    {
+//        id = p.Id,
+//        title = p.Title,
+//        ViewCount = p.ViewCount,
+//        PosterDate = p.PostedDate,
+//        Author = p.Author.FullName,
+//        Category = p.Category.Name
+//    })
+//    .ToList();
 
 foreach (var post in posts)
 {
-    Console.WriteLine("ID       : {0}", post.id);
-    Console.WriteLine("Title    : {0}", post.title);
+    Console.WriteLine("ID       : {0}", post.Id);
+    Console.WriteLine("Title    : {0}", post.Title);
     Console.WriteLine("View     : {0}", post.ViewCount);
-    Console.WriteLine("Date     : {0:MM/dd/yyyy}", post.PosterDate);
-    Console.WriteLine("Author   : {0}", post.Author);
-    Console.WriteLine("Category : {0}", post.Category);
+    Console.WriteLine("Date     : {0:MM/dd/yyyy}", post.PostedDate);
+    Console.WriteLine("Author   : {0}", post.Author.FullName);
+    Console.WriteLine("Category : {0}", post.Category.Name);
     Console.WriteLine("".PadRight(80, '-'));
 
 }
-Console.WriteLine("{0.-4}{1.-30}{2.-30}{3.12}", "ID", "Full Name", "Email", "Joined Date");
+//Console.WriteLine("{0.-4}{1.-30}{2.-30}{3.12}", "ID", "Full Name", "Email", "Joined Date");
 
 foreach (var author in authors)
 {
